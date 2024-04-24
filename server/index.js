652ed63b98de08760f3be5ee9a021bb673a91495
AbuUsaid <usaidabu321@gmail.com>
2024-04-24 11:37:13 +0530

    connect to db

--- server/index.js
+++ server/index.js
@@ -1,12 +1,22 @@
 require('dotenv').config();
 const cors = require('cors');
 const express = require('express');
+const connectDB = require('./connectDB');
 
 const app = express();
 const PORT = process.env.PORT || 8000;
 
+connectDB();
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.json('Hello!');
+});
+
+app.get('*', (req, res) => {
+  res.sendStatus('404');
 });
 
 app.listen(PORT, () => {
