a7557b31f7a51489bb220eb9f4e02adb3100ac0a
AbuUsaid <usaidabu321@gmail.com>
2024-07-23 08:21:04 +0530

    fetch data and display notes

--- client/src/components/Notes.jsx
+++ client/src/components/Notes.jsx
@@ -1,7 +1,59 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
 function Notes() {
-  return <div>Notes 123</div>;
+  const baseUrl = `${import.meta.env.VITE_SERVER_URL}/api/notes`;
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(baseUrl);
+
+        if (!response.ok) {
+          throw new Error('Failed to fetch data.');
+        }
+
+        const data = await response.json();
+        setData(data);
+        setIsLoading(false);
+      } catch (error) {
+        setError('Error fetching data. Please try again later.');
+        setIsLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      {/* <pre>{JSON.stringify(data, null, 2)}</pre> */}
+
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : error ? (
+        <p>{error}</p>
+      ) : (
+        <ul className="notes">
+          {data.map((item) => (
+            <li key={item._id}>
+              <Link to={`/note/${item._id}`}>
+                <h3>{item.title}</h3>
+                <p>
+                  {item.description.length > 50
+                    ? `${item.description.substring(0, 50)}...`
+                    : item.description}
+                </p>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
 }
 
 export default Notes;
+``;
